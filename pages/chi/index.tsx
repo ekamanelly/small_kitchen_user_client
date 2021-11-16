3150859d4c01bafc80df03cfc503aaf63937bbe5
ekamanelly <ekamanelly@gmail.com>
2021-11-16 21:24:09 +0100

    :lipstick: [fix] ui glish

--- pages/chi/index.tsx
+++ pages/chi/index.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import "../../styles/chiStyle.css";
 
-function VendorProfile() {
+function VendorProfileFeedBack() {
   return <div>chika put your html mark up here</div>;
 }
 
-export default VendorProfile;
+export default VendorProfileFeedBack;
