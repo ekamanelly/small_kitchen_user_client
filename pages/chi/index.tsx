68ce29d66ede725f9423c3c4d1cda0792ff1f358
ekamanelly <ekamanelly@gmail.com>
2021-11-16 22:22:25 +0100

    :lipstick: connected style sheet to view

--- pages/chi/index.tsx
+++ pages/chi/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import style from "../../styles/chiStyle.module.css";
 function VendorProfileFeedBack() {
-  return <div className="chika">chika put your html mark up here</div>;
+  return (
+    <div className={style.chika}>
+      <h1>chika put your html mark up here</h1>
+      <h1>
+        ../../styles/chiStyle.module.css trace this file path to see the CSS
+        sheet that is connected to this view
+      </h1>
+      <h1>use className instead of class, it what React understands </h1>
+      <p>
+        some concept you might not get initially but you will... do well to let
+        know about your challenges, google help too{" "}
+      </p>
+      <h1>https://www.youtube.com/watch?v=nyB7MlYo1v0</h1>
+    </div>
+  );
 }
 
 export default VendorProfileFeedBack;
