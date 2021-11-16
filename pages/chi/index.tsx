33d4dfbd6b66dd2af556cbae4d459a4ceffb47f2
ekamanelly <ekamanelly@gmail.com>
2021-11-16 20:48:48 +0100

    :lipstick: [added] profileFeedbackPage for Chika to finish

---
+++ pages/chi/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../styles/chiStyle.css";
+
+function VendorProfile() {
+  return <div>chika put your html mark up here</div>;
+}
+
+export default VendorProfile;
