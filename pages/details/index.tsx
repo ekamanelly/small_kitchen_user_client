6c86b8759f89fa35df13fb7cc65868f84ab9907c
Martins Ngene <martinsngene9@gmail.com>
2021-11-16 21:19:54 +0100

    [update]

--- pages/details/index.tsx
+++ pages/details/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import NavBar from '../../components/NavBar';
+import Image from "next/image";
+import React from "react";
+import Grill from "../../public/media/Grill.png";
+// import NavBar from '../../components/NavBar';
 
 function FoodDetails() {
     return (
         <div>
-            <NavBar />
+            <div className="container">
+                <div className="row align-items-center justify-content-center">
+                    <div className="col-md-10">
+                        <div className="img-fluid">
+                            <Image
+                                src={Grill}
+                                alt="photo"
+                             />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     )
 }
 
