89a7a0cdc4b0bb3fe056b83ffbdee187a1466be4
ekamanelly <ekamanelly@gmail.com>
2021-11-22 20:25:14 +0100

    :ambulance: working on image path

--- components/GettingStarted.tsx
+++ components/GettingStarted.tsx
@@ -13,9 +13,9 @@ function GettingStarted() {
                 Get started in <span>3 easy steps</span>
               </div>
               <div className="steps">
-                <div>1</div>Lorem ipsum, dolor sit amet consectetur adipisicing
-                elit. Libero a debitis suscipit expedita voluptates, optio
-                laboriosam inventore labore asperiores error.
+                <div>1</div>Lorem ipsum, dolor sit ascsdmet consectetur
+                adipisicing elit. Libero a debitis suscipit expedita voluptates,
+                optio laboriosam inventore labore asperiores error.
               </div>
               <div className="steps">
                 <div>2</div>Lorem ipsum, dolor sit amet consectetur adipisicing
